15f490f3c1a50409d3eda7e4857c5bf7bcad951f
Guillaume Frichet <guillaume.frichet@gmail.com>
2014-07-06 23:38:07 +0200

    b

--- public/js/hand.js
+++ public/js/hand.js
@@ -8,50 +8,80 @@ var Hand = function(cards) {
 		.y(stage.getHeight() / 2);
 
 	cards.forEach(function(card, key, _deck) {
-		var card = new Card(card.rank, card.type), zIndex,
+		var card = new Card(card.rank, card.type),
+			tween, zIndex,
 			angle = key - Math.floor(cards.length / 2),
 			posX = key * 20,
 			posY = Math.abs(angle);
 
-		card.init = function() {
-			this.node.scale({x:1,y:1})
-			.x( posX )
-			.y( posY )
-			.rotate( angle );
-		};
+		cards[key] = card;
 
-		card.init();
+		card.node.scale({x:1,y:1})
+			.x( posX )
+			.y( posY );
 
 		card.node
+			.rotate( angle )
 			.on('mouseover', function(e){
+				document.body.style.cursor = 'pointer';
 				if (zIndex === undefined) {
 					zIndex = card.node.getZIndex();
 				}
-				card.node
-					.y( e.target.getY() - 20 )
-					.moveToTop();//.setZIndex(deck.length + 1);
-				document.body.style.cursor = 'pointer';
+				card.node.moveToTop();//.setZIndex(deck.length + 1);
 
-				/*deck.getChildren().each(function(node) {
-					if (node !== card.node) {
-						node.x( node.getX() - 50 );
-					}
-				});*/
+				// cards.forEach(function(c, k) {
+					// if (node !== card.node) {
+					// 	node.x( node.getX() - 50 );
+					// }
+					//console.log(key, k);
+					// if (k < key) {
+					// 	new Kinetic.Tween({
+					// 		node: c.node,
+					// 		duration: 0.3,
+					// 		x: e.target.getX() - 10
+					// 	}).play();
+					// } else if (k > key) {
+					// 	new Kinetic.Tween({
+					// 		node: c.node,
+					// 		duration: 0.3,
+					// 		x: e.target.getX() + 10
+					// 	}).play();
+					// }
+				// });
+				new Kinetic.Tween({
+					node: card.node,
+					duration: 0.2,
+					y: e.target.getY() - 20
+				}).play();
 			})
 			.on('mouseout', function(e){
-				card.node
-					.y( e.target.getY() )
-					.setZIndex(zIndex);
 				document.body.style.cursor = 'default';
+				card.node.setZIndex( zIndex );
+				new Kinetic.Tween({
+					node: card.node,
+					duration: 0.2,
+					x: posX,
+					y: posY
+				}).play();
 			})
 			.on('dragstart', function(e){
-				e.target
-					.scale({x:1.2,y:1.2})
-					.moveToTop();
+				new Kinetic.Tween({
+					node: card.node,
+					duration: 0.1,
+					scaleX: 1.2,
+					scaleY: 1.2
+				}).play();
 				//console.log({x:e.target.x(), y:e.target.y()});
 			})
 			.on('dragend', function(e){
-				card.init();
+				tween = new Kinetic.Tween({
+					node: card.node,
+					duration: 0.1,
+					x: posX,
+					y: posY,
+					scaleX: 1,
+					scaleY: 1
+				}).play();
 				e.target
 					.setZIndex( zIndex );
 				//deckLayer.draw();
